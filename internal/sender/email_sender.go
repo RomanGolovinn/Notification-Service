3615381cb6db415e48568419dc0216fb577969ae
Roman Golovin <golovinroman.ev@gmail.com>
2025-12-28 16:17:22 +0300

    fech add logic for notification sender

--- internal/sender/email_sender.go
+++ internal/sender/email_sender.go
@@ -1,1 +1,45 @@
 package sender
+
+import (
+	"context"
+	"fmt"
+	"net/smtp"
+
+	"Notification-Service/internal/service"
+)
+
+type EmailSender struct {
+	config EmailConfig
+	//implement NotificationSenders
+}
+
+type EmailConfig struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+	From     string
+}
+
+func NewEmailSender(config EmailConfig) *EmailSender {
+	return &EmailSender{config: config}
+}
+
+func (s *EmailSender) Send(ctx context.Context, notification service.Notification) error {
+	subject := "Уведомление"
+	mime := "Mime-Version: 1.0;\nContent-Type: text; charset=\"UTF-8\";\n\n"
+	msg := []byte(subject + mime + notification.Message)
+
+	auth := smtp.PlainAuth("", s.config.Username, s.config.Password, s.config.Host)
+	addr := fmt.Sprintf("%s:%d", s.config.Host, s.config.Port)
+	err := smtp.SendMail(addr, auth, s.config.From, []string{notification.Address}, msg)
+
+	if err != nil {
+		return fmt.Errorf("failed to send email to %s: %w", notification.Address, err)
+	}
+	return nil
+}
+
+func (s *EmailSender) Type() service.NotificationType {
+	return service.Email
+}
