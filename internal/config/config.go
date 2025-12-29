71f10cea5fb94db48abec75a6610104d972871de
Roman Golovin <golovinroman.ev@gmail.com>
2025-12-29 20:14:34 +0300

    fech: added config.go (parser for config) fech: added example for config.json fech: added .gitignore

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,30 @@
 package config
 
-// config
+import (
+	"encoding/json"
+	"os"
+
+	"Notification-Service/internal/sender"
+)
+
+type Config struct {
+	ServerPort string             `json:"server_port"`
+	DBDSN      string             `json:"db_dsn"`
+	Email      sender.EmailConfig `json:"email"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var cfg Config
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
