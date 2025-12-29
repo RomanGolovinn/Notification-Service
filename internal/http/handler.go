c434a07b85602cee4fa7e6874545c37b273cbc04
Roman Golovin <golovinroman.ev@gmail.com>
2025-12-29 13:29:19 +0300

    fech: created http handler

--- internal/http/handler.go
+++ internal/http/handler.go
@@ -1,3 +1,44 @@
 package http
 
-// handler
+import (
+	"encoding/json"
+	"net/http"
+
+	"Notification-Service/internal/service"
+)
+
+type NotificationHandler struct {
+	service *service.NotificationService
+}
+
+func NewNotificationHandler(service *service.NotificationService) *NotificationHandler {
+	return &NotificationHandler{service: service}
+}
+
+func (h *NotificationHandler) Create(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		// can handle only post requests
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var notification service.Notification
+	err := json.NewDecoder(r.Body).Decode(&notification)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	err = h.service.Handle(r.Context(), &notification)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]any{
+		"status": "success",
+		"id":     notification.ID,
+	})
+}
